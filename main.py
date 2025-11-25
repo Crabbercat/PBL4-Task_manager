a514d0c730a391e7394d3743a99a1cbb0a29db21
Crabbercat <102230292@sv1.dut.udn.vn>
2025-11-25 22:02:10 +0700

    nah

--- main.py
+++ main.py
@@ -13,8 +13,7 @@ frontend_origins = [
     "http://localhost",
     "http://127.0.0.1",
     "http://localhost:5173",
-    "http://127.0.0.1:5173",
-    "http://192.168.1.60",
+    "http://127.0.0.1:5173"
 
 ]
 
